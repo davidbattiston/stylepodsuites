977af7205d9d91d37b85b184fef349d1164c892b
Dave <itsalldave@gmail.com>
2024-10-09 13:20:41 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -67,7 +67,7 @@
                     <span> <b>107</b> - Sara Baltzegar Hair LLC</span>
                 </li>
                 <li>
-                    <span> <b>109</b> - Available</span>
+                    <span> <b>109</b> - Carytown Massage</span>
                 </li>
                 <li>
                     <span> <b>110</b> - Style by Karen</span>
